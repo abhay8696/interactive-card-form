87d0b0154ef00d3367ce179deb5b51e80d5ea556
abhay8696 <abhaykamble1346@gmail.com>
2022-09-06 21:42:36 +0530

    created hmtl skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import front from './images/bg-card-front.png';
+import back from './images/bg-card-back.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          abhay8696
-        </a>
-      </header>
+      <img src={front} className='front'></img>
+      <img src={back} className='back'></img>
+      <div className='grad'></div>
+      <div className='formDiv'>
+        <form className='form'>
+          <div className='nameDiv'>
+          <label htmlFor="name">CARDHOLDER NAME</label>
+          <input id="name" placeholder='Jane Appleseed'/>
+          </div>
+
+          <div className='numberDiv'>
+          <label htmlFor="number" type="number">CARDHOLDER NUMBER</label>
+          <input id="number" placeholder='1234 5678 0000'/>
+          </div>
+
+          <div className='dateDiv'>
+            <label htmlFor="mm">EXP. DATE(MM/YY)</label>
+            <input id="mm" placeholder='MM'/>
+            <input id="yy" placeholder='YY'/>
+
+            <label htmlFor="cvc">CVC</label>
+            <input id='cvc' placeholder='e.g. 123'/>
+          </div>
+
+          <div type="submit">Confirm</div>
+        </form>
+      </div>
     </div>
   );
 }
