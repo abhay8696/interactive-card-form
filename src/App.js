cf626c87d98fc3921f65515b8513587a9baec2f4
abhay8696 <abhaykamble1346@gmail.com>
2022-09-07 20:02:13 +0530

    card styles for desktop screen completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import front from './images/bg-card-front.png';
 import back from './images/bg-card-back.png';
+import logo from './images/card-logo.svg'
 
 function App() {
   //states
@@ -15,17 +16,26 @@ function App() {
   return (
     <div className="App">
       <div className='frontDiv'>
-        <div className='frontData'>
-          <span>{cardNumber}</span>
-          <span>{name}</span>
-          <span>{month}</span>
-          <span>{year}</span>
-          <span>{cvc}</span>
+        <div className='frontSide'>
+          <img src={logo} className='cardLogo'/>
+          <div className='frontData'>
+            <div className='cardNum'>{cardNumber?.length ? cardNumber : '0000 0000 0000 0000'}</div>
+            <div className='name-date'>
+              <span>{name?.length ? name : 'Jane Appleseed'}</span>
+              <div>
+                <span>{month?.length ? month : 'MM'}</span>/
+                <span>{year?.length ? year : 'YY'}</span>
+              </div>
+            </div>
+          </div>
         </div>
-        <img src={front} className='front'></img>
+        <img src={front} className='frontImg'></img>
       </div>
       <div className='backDiv'>
-        <img src={back} className='back'></img>
+        <div className='backSide'>
+          <span>{cvc?.length ? cvc : '000'}</span>
+        </div>
+        <img src={back} className='backImg'></img>
       </div>
       <div className='grad'></div>
       <div className='formDiv'>
@@ -39,29 +49,34 @@ function App() {
           <label htmlFor="number" type="number">CARDHOLDER NUMBER</label>
           <input 
             id="number" 
-            placeholder='1234 5678 0000' 
+            placeholder='0000 0000 0000' 
             onChange={e=> setCardNumber(e.target.value)}/>
           </div>
 
-          <div className='dateDiv'>
-            <label htmlFor="mm">EXP. DATE(MM/YY)</label>
-            <input 
-              id="mm" 
-              placeholder='MM'
-              onChange={e=> setMonth(e.target.value)}
-            />
-            <input 
-              id="yy" 
-              placeholder='YY'
-              onChange={e=> setYear(e.target.value)}
-            />
-
-            <label htmlFor="cvc">CVC</label>
-            <input 
-              id='cvc' 
-              placeholder='e.g. 123'
-              onChange={e=> setCvc(e.target.value)}
-            />
+          <div className='date-cvc'>
+            <div className='dateDiv'>
+              <label htmlFor="mm">EXP. DATE(MM/YY)</label>
+              <div>
+                <input 
+                  id="mm" 
+                  placeholder='MM'
+                  onChange={e=> setMonth(e.target.value)}
+                />
+                <input 
+                  id="yy" 
+                  placeholder='YY'
+                  onChange={e=> setYear(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className='cvcDiv'>
+              <label htmlFor="cvc">CVC</label>
+              <input 
+                id='cvc' 
+                placeholder='e.g. 123'
+                onChange={e=> setCvc(e.target.value)}
+              />
+            </div>
           </div>
 
           <div type="submit">Confirm</div>
