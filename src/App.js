42caeaf135b9dbe04f99952a58cb2d8744df5485
abhay8696 <abhaykamble1346@gmail.com>
2022-09-11 18:17:48 +0530

    worked on error reports generated by front-end-mentor..

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1></h1>
+      <h1>Interactive Card Form</h1>
       <div className='backDiv'>
         <div className='backSide'>
           <span>{cvc?.length ? cvc : '000'}</span>
